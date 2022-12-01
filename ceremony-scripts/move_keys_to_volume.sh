8bd406c557e9fcc8d6eb025d1667eed6129c3c4b
Jordan Brown <jmbrown412@gmail.com>
2022-11-30 17:44:04 -0700

    move keys to volume

---
+++ ceremony-scripts/move_keys_to_volume.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+EXTERNAL_VOLUME_PATH=$1 # Example - /dev/sdc1
+VOLUME_CONTENTS_PATH=$2 # Example - ../volumes/volume1
+
+echo "Moving keys from "$VOLUME_CONTENTS_PATH" to "$EXTERNAL_VOLUME_PATH""
+
+read -p "Please insert the usb drive for $VOLUME_CONTENTS_PATH and hit enter"
+
+umount $EXTERNAL_VOLUME_PATH
+
+sudo mkfs.exfat $EXTERNAL_VOLUME_PATH
+
+sudo fsck $EXTERNAL_VOLUME_PATH
+
+mv $VOLUME_CONTENTS_PATH $EXTERNAL_VOLUME_PATH

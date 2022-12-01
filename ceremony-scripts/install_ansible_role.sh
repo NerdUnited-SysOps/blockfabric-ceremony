6f0cf4f779e59ad3fca2db927c38178934504c2c
vagrant <adamjtew@gmail.com>
2022-12-01 06:22:09 -0500

    Fixed token

--- ceremony-scripts/install_ansible_role.sh
+++ ceremony-scripts/install_ansible_role.sh
@@ -5,7 +5,7 @@ ${SCRIPTS_DIR}/print_title.sh "Installing Ansible role"
 BASE_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 source .common.sh
 
-ansible-galaxy install git+https://${GIT_TOKEN}:@github.com/NerdUnited-Nerd/ansible-role-lace,${ANSIBLE_ROLE_LACE_VERSION} --force
+ansible-galaxy install git+https://${GITHUB_SYSOPS_TOKEN}:@github.com/NerdUnited-Nerd/ansible-role-lace,${ANSIBLE_ROLE_LACE_VERSION} --force
 
 if [ ! $? -eq 0 ]; then
    ${SCRIPTS_DIR}/print_error.sh "Failed to install ansible role"

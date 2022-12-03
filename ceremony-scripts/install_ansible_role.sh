e643b221199f1b1aebc6eabcaa6ebe518f08deac
Jordan Brown <jmbrown412@gmail.com>
2022-12-02 17:00:31 -0700

    variablized github org and repo

--- ceremony-scripts/install_ansible_role.sh
+++ ceremony-scripts/install_ansible_role.sh
@@ -5,7 +5,12 @@ ${SCRIPTS_DIR}/printer.sh -t "Installing Ansible role"
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 source ${SCRIPT_DIR}/../.common.sh
 
-ansible-galaxy install git+https://${GITHUB_SYSOPS_TOKEN}:@github.com/NerdUnited-Nerd/ansible-role-lace,${ANSIBLE_ROLE_LACE_VERSION} --force
+GITHUB_ORG_URL=${1:github.com/NerdUnited-Nerd}
+REPO_NAME=${2:ansible-role-lace}
+
+REPO_NAME=ansible.${BRAND}-${NETWORK}
+
+ansible-galaxy install git+https://${GITHUB_SYSOPS_TOKEN}:@${GITHUB_ORG_URL}/${REPO_NAME},${ANSIBLE_ROLE_LACE_VERSION} --force
 
 if [ ! $? -eq 0 ]; then
    ${SCRIPTS_DIR}/printer.sh -e "Failed to install ansible role"

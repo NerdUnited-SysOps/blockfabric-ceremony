dad316b9e6d470da0edb8c91a60f096de51c3f36
Adam <adamjtew@gmail.com>
2022-12-05 16:00:43 -0500

    Added source

--- ceremony-scripts/move_keys_to_volume.sh
+++ ceremony-scripts/move_keys_to_volume.sh
@@ -3,6 +3,10 @@
 EXTERNAL_VOLUME_PATH=$1 # Example - /dev/sdc1
 VOLUME_CONTENTS_PATH=$2 # Example - ../volumes/volume1
 
+
+SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+source $SCRIPT_DIR/../.common.sh
+
 ${SCRIPTS_DIR}/print_title.sh "Moving keys from "$VOLUME_CONTENTS_PATH" to "$EXTERNAL_VOLUME_PATH""
 
 read -p "Please insert the usb drive for $VOLUME_CONTENTS_PATH and hit enter"

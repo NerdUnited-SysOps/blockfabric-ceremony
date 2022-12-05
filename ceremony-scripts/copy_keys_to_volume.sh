dcdf60245f8a39d57a7b9090ef467b649fff6818
jsanders5 <jtsanders87@gmail.com>
2022-12-05 14:35:32 -0700

    changed reference from move to copy

--- ceremony-scripts/copy_keys_to_volume.sh
+++ ceremony-scripts/copy_keys_to_volume.sh
@@ -6,7 +6,7 @@ VOLUME_CONTENTS_PATH=$2 # Example - ../volumes/volume1
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 source $SCRIPT_DIR/../.common.sh
 
-${SCRIPTS_DIR}/printer.sh -t "Moving keys from "$VOLUME_CONTENTS_PATH" to "$EXTERNAL_VOLUME_PATH""
+${SCRIPTS_DIR}/printer.sh -t "Copying keys from "$VOLUME_CONTENTS_PATH" to "$EXTERNAL_VOLUME_PATH""
 
 read -p "Please insert the usb drive for $VOLUME_CONTENTS_PATH and hit enter"
 
@@ -16,5 +16,5 @@ umount $EXTERNAL_VOLUME_PATH
 
 sudo fsck $EXTERNAL_VOLUME_PATH
 
-mv $VOLUME_CONTENTS_PATH $EXTERNAL_VOLUME_PATH
+cp $VOLUME_CONTENTS_PATH $EXTERNAL_VOLUME_PATH
 

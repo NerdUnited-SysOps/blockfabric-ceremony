d10b3baa46b8a45959bd4b44e3b95aaee5e1da0b
ceremony-script <ceremony@email.com>
2022-12-06 04:30:51 -0500

    Cleaned up common.sh

--- .common.sh
+++ .common.sh
@@ -1,17 +1,4 @@
 # Versioning for dependencies
-export APT_NODEJS_VERSION=18.10.0+dfsg-6
-export APT_NPM_VERSION=9.1.2~ds1-2
-export APT_GO_VERSION=2:1.19~1
-export APT_JQ_VERSION=1.6-2.1
-export APT_PWGEN_VERSION=2.08-2
-export APT_AWSCLI_VERSION=1.24.8-1
-export ETHKEY_VERSION=v1.10.26
-export GETH_VERSION=v1.10.26
-export ANSIBLE_ROLE_LACE_VERSION=1.0.0.5-test
-
-# Contract versions
-export DAO_VERSION=v0.0.1
-export LOCKUP_VERSION=v0.1.0
 
 # Common paths to be used throughout the various scripts
 export BASE_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
@@ -24,20 +11,10 @@ export ANSIBLE_DIR=${BASE_DIR}/../ansible
 export LOG_FILE=${BASE_DIR}/log
 export ENV_FILE=${BASE_DIR}/.env
 
-# Pathing for additional binaries
 export PATH="${HOME}/go/bin:${PATH}"	 # Go binary path
-
-# Interacting with AWS Secrets Manager
-# For simplicity, let's use this same Key in AWS Secrets Mgr for retrieving the SSH Key.
-
 export SCP_USER=admin
 export INVENTORY_PATH=${ANSIBLE_DIR}/inventory
 export REMOTE_INVENTORY_PATH=/opt/blockfabric/inventory
-
-# Output coloring
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m'
 
 # Overrides based on the environment
 if [ -f .env ]
--- ceremony-scripts/install_dependencies.sh
+++ ceremony-scripts/install_dependencies.sh
@@ -5,15 +5,28 @@ set -e
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 source ${SCRIPT_DIR}/../.common.sh
 
-function print_status() {
-  package=$1
-	version=$2
+APT_NODEJS_VERSION=18.10.0+dfsg-6
+APT_NPM_VERSION=9.1.2~ds1-2
+APT_GO_VERSION=2:1.19~1
+APT_JQ_VERSION=1.6-2.1
+APT_PWGEN_VERSION=2.08-2
+APT_AWSCLI_VERSION=1.24.8-1
+ETHKEY_VERSION=v1.10.26
+GETH_VERSION=v1.10.26
+ANSIBLE_ROLE_LACE_VERSION=1.0.0.5-test
 
-	if dpkg -l ${package} &>> ${LOG_FILE}; then
+function print_status() {
+	binary=$1
+  package=$2
+	version=$3
+
+	if which ${binary} &>> ${LOG_FILE}; then
 		${SCRIPTS_DIR}/printer.sh -n "${package} Already installed, skipping..."
 	else
-		if sudo apt install -y ${package}=${version}; then
-			${SCRIPTS_DIR}/printer.sh -n "Installed ${package}"
+		${SCRIPTS_DIR}/printer.sh -n "${package} not found, installing"
+
+		if sudo apt install -y ${package}=${version} &>> ${LOG_FILE}; then
+			${SCRIPTS_DIR}/printer.sh -s "Installed ${package}"
 		else
 			${SCRIPTS_DIR}/printer.sh -e "Failed to install ${package}"
 		fi
@@ -24,24 +37,20 @@ function print_status() {
 ${SCRIPTS_DIR}/printer.sh -t "Installing dependencies" | tee -a ${LOG_FILE}
 ${SCRIPTS_DIR}/printer.sh -n "This may take a minute..." | tee -a ${LOG_FILE}
 
-
 sudo apt-get update &>> ${LOG_FILE}
 ${SCRIPTS_DIR}/printer.sh -n "Updated system"
 
-print_status "nodejs" ${APT_NODEJS_VERSION}
-print_status "npm" ${APT_NPM_VERSION}
-print_status "awscli" ${APT_AWSCLI_VERSION}
-print_status "pwgen" ${APT_PWGEN_VERSION}
-print_status "jq" ${APT_JQ_VERSION}
-print_status "golang" ${APT_GO_VERSION}
-print_status "ansible" ""
-print_status "curl" ""
+print_status "node" "nodejs" ${APT_NODEJS_VERSION}
+print_status "npm" "npm" ${APT_NPM_VERSION}
+print_status "aws" "awscli" ${APT_AWSCLI_VERSION}
+print_status "pwgen" "pwgen" ${APT_PWGEN_VERSION}
+print_status "jq" "jq" ${APT_JQ_VERSION}
+print_status "go" "golang" ${APT_GO_VERSION}
+print_status "ansible" "ansible" ""
+print_status "curl" "curl" ""
 
 go install github.com/ethereum/go-ethereum/cmd/ethkey@${ETHKEY_VERSION} &>> ${LOG_FILE}
-# print_status "ethkey"
-
 go install github.com/ethereum/go-ethereum/cmd/geth@${GETH_VERSION} &>> ${LOG_FILE}
-# print_status "geth"
 
 ${SCRIPTS_DIR}/printer.sh -s "Installed dependencies" | tee -a ${LOG_FILE}
 

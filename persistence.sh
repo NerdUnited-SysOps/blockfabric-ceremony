0133ea2f0266f93ee176c8e83faccaee12c304ff
jasonleckienu <113492785+jasonleckienu@users.noreply.github.com>
2023-03-06 13:55:59 -0700

    prepend bootstrap.log
    Prepend bootstrap.log into ceremony.log before github commits and copies to volumes

--- persistence.sh
+++ persistence.sh
@@ -97,6 +97,12 @@ upsert_file() {
 }
 
 save_ansible_vars() {
+	# Prepend bootstrap.log into ceremony.log before github commits and copies to volumes
+	x=$(cat ~/bootstrap.log; cat ${LOG_FILE})
+	echo "$x" > ${LOG_FILE}.combined
+	mv ${LOG_FILE} ${LOG_FILE}.orig
+	mv ${LOG_FILE}.combined ${LOG_FILE}
+	
 	cp ${LOG_FILE} ${ANSIBLE_DIR}/ceremony.log
 	git config --global user.name "ceremony-script"
 	git config --global user.email "ceremony@email.com"

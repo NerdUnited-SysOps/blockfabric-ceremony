8464eca7e102b31dcc19b8450d133e5df32435bb
Adam <adamjtew@gmail.com>
2023-04-12 10:00:14 -0600

    fix: pathing for distribution issuer

--- ceremony-scripts/deploy_bridge_contracts.sh
+++ ceremony-scripts/deploy_bridge_contracts.sh
@@ -82,8 +82,8 @@ get_address() {
 get_deployer_a_private_key() {
     keystore=$(${SCRIPTS_DIR}/get_aws_key.sh "${AWS_DISTIRBUTION_ISSUER_KEYSTORE}")
     keystore_file_path=./tmp/issuer_keystore
+    mkdir -p ./tmp
 
-    mkdir -p ${keystore_file_path}
     echo "${keystore}" > ${keystore_file_path}
 
     password=$(${SCRIPTS_DIR}/get_aws_key.sh "${AWS_DISTIRBUTION_ISSUER_PASSWORD}")

c3da27ff3d8c7e2440918468c8fd3c405523fad3
jasonleckienu <113492785+jasonleckienu@users.noreply.github.com>
2023-04-13 16:05:03 -0600

    pre-pend bootstrap.log correctly
    the pre-pend should occur in save_log_file, not save_ansible_vars

--- persistence.sh
+++ persistence.sh
@@ -99,15 +99,8 @@ upsert_file() {
 save_ansible_vars() {
 	[ -d ${ANSIBLE_DIR} ] || git clone ${BRAND_ANSIBLE_URL} ${ANSIBLE_DIR} 
 	now=$(date +"%m_%d_%y")
-	# Prepend bootstrap.log into ceremony.log before github commits and before it's copied to volumes/
-	if [ -f "${HOME}/bootstrap.log" ]; then
-		COMBINED=$(cat ${HOME}/bootstrap.log; cat ${LOG_FILE})
-		echo "$COMBINED" > "${LOG_FILE}"
-	fi
-
-	echo "Finished: $(date)" >> "${LOG_FILE}"
-	## bootstrap.log will already have a Started: timestamp
-	cp "${LOG_FILE}" "${ANSIBLE_DIR}/ceremony_${now}.log"
+	##	
+	cp "${LOG_FILE}" "${ANSIBLE_DIR}/${now}_ceremony.log"
 	git config --global user.name "ceremony-script"
 	git config --global user.email "ceremony@email.com"
 	git -C ${ANSIBLE_DIR}/ checkout -B "ceremony-artifacts-${now}"
@@ -121,6 +114,15 @@ save_ansible_vars() {
 save_log_file() {
 	printer -t "Copying ${LOG_FILE} file to all volumes"
 	echo "\n"
+
+	### Prepend bootstrap.log into ceremony.log before github commits and before it's copied to volumes/
+	if [ -f "${HOME}/bootstrap.log" ]; then
+		COMBINED=$(cat ${HOME}/bootstrap.log; cat ${LOG_FILE})
+		echo "$COMBINED" > "${LOG_FILE}"
+	fi
+
+	echo "Finished: $(date)" >> "${LOG_FILE}"
+	### bootstrap.log will already have a Started: timestamp
 
 	now=$(date +"%m_%d_%y")
 

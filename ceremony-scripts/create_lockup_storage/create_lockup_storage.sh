0fbc3275942ef71654b0ffd6ca21edc8b866f2e9
Adam <adamjtew@gmail.com>
2023-10-16 10:50:33 -0600

    Accepting args in create_lockup_storage.sh

--- ceremony-scripts/create_lockup_storage/create_lockup_storage.sh
+++ ceremony-scripts/create_lockup_storage/create_lockup_storage.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env zsh
 
+usage() {
+	echo "Options"
+	echo "  -e : Envifonment config file"
+	echo "  -h : This help message"
+}
+
+while getopts e:h option; do
+	case "${option}" in
+		e) ENV_FILE=${OPTARG};;
+		h)
+			usage
+			exit 0
+			;;
+	esac
+done
+
 if [ ! -f "${ENV_FILE}" ]; then
 	echo "${ZSH_ARGZERO}:${0}:${LINENO} Missing .env file. Expected it here: ${ENV_FILE}"
 	exit 1
 else
 	source ${ENV_FILE}
 fi
+
+SCRIPT_DIR="${SCRIPTS_DIR}/create_lockup_storage"
 
 VOL=${VOLUMES_DIR}/volume1/lockupAdmins
 

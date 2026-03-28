49f18ce894ed2993a3398c800817e034f92b4b02
paulgs9988 <paulgs9988@gmail.com>
2026-03-28 13:23:57 -0600

    updating tags for AW chain and bridge_x deployment

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,9 @@
 
 version="2.3.2"
 chain_repo_tag="2.1.1"
-additions_repo_tag="2.7.4"
+additions_repo_tag="2.8.0"
 ansible_repo_tag="main"
-ceremonyenv_repo_tag="2.7.4"
+ceremonyenv_repo_tag="2.8.0"
 ceremony_os_version=$(cat ${HOME}/version | tail -2)
 export network=$1
 export chain=$2

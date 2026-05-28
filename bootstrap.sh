f2a66de2837ff1873de70a47ad41d645caacf41e
paulgs9988 <69936006+paulgs9988@users.noreply.github.com>
2026-05-28 13:19:49 -0600

    Bump additions and ceremonyenv repo tags to 2.10.6
    Updated additions and ceremonyenv repository tags to version 2.10.6.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,9 @@
 
 version="2.3.8"
 chain_repo_tag="2.3.2"
-additions_repo_tag="2.10.5"
+additions_repo_tag="2.10.6"
 ansible_repo_tag="main"
-ceremonyenv_repo_tag="2.10.5"
+ceremonyenv_repo_tag="2.10.6"
 ceremony_os_version=$(cat ${HOME}/version | tail -2)
 export network=$1
 export chain=$2
